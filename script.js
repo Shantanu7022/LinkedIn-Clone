9eb42e0c3f5622ad9ab215bb07b5089b32b2adf6
Shantanu Gayen <shantanugayen200207@gmail.com>
2024-01-28 10:55:55 +0530

    commit changes

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ let profileMenu = document.getElementById("profileMenu");
 function toggleMenu() {
     profileMenu.classList.toggle("open-menu");
 }
+
+
+let sidebarActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
+
+function toggleActivity(){
+    sidebarActivity.classList.toggle("open-activity");
+    if(sidebarActivity.classList.contains("open-activity")){
+        moreLink.innerHTML = "Show less <b>-</b>"
+    }else{
+        moreLink.innerHTML = "Show more <b>+</b>"
+    }
+}
