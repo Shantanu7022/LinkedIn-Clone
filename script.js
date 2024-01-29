41cea9c4ac5ad094d54d2f8a385fbe4712e1242a
Shantanu Gayen <shantanugayen200207@gmail.com>
2024-01-29 20:05:06 +0530

    commit changes

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function toggleMenu() {
 let sidebarActivity = document.getElementById("sidebarActivity");
 let moreLink = document.getElementById("showMoreLink");
 
-function toggleActivity(){
+function toggleActivity() {
     sidebarActivity.classList.toggle("open-activity");
-    if(sidebarActivity.classList.contains("open-activity")){
+    if (sidebarActivity.classList.contains("open-activity")) {
         moreLink.innerHTML = "Show less <b>-</b>"
-    }else{
+    } else {
         moreLink.innerHTML = "Show more <b>+</b>"
     }
 }
